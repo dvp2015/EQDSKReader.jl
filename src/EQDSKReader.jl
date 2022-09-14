12953364b217b29dd5c11156fe3eff3d52bb6182
dvp <dmitri_portnov@yahoo.com>
2022-09-14 19:21:54 +0300

    feat: implemented reading of EQDSK file

--- src/EQDSKReader.jl
+++ src/EQDSKReader.jl
@@ -1,5 +1,195 @@
+"""
+    EQDSKReader
+
+Functionality to read EQDSK file.
+
+From "G_EQDSK.pdf":
+
+"...Briefly, a right-handed
+cylindrical coordinate system (R, φ, Ζ) is used. The G EQDSK provides
+information on the pressure, poloidal current function, q profile on a uniform flux
+grid from the magnetic axis to the plasma boundary and the poloidal flux
+function on the rectangular computation grid. Information on the plasma
+boundary and the surrounding limiter contour in also provided."
+
+```code:
+        character*10 case(6)
+        dimension psirz(nw,nh),fpol(1),pres(1),ffprim(1),
+
+        pprime(1),qpsi(1),rbbbs(1),zbbbs(1),
+
+        rlim(1),zlim(1)
+        c
+        read (neqdsk,2000) (case(i),i=1,6),idum,nw,nh
+        read (neqdsk,2020) rdim,zdim,rcentr,rleft,zmid
+        read (neqdsk,2020) rmaxis,zmaxis,simag,sibry,bcentr
+        read (neqdsk,2020) current,simag,xdum,rmaxis,xdum
+        read (neqdsk,2020) zmaxis,xdum,sibry,xdum,xdum
+        read (neqdsk,2020) (fpol(i),i=1,nw)
+        read (neqdsk,2020) (pres(i),i=1,nw)
+        read (neqdsk,2020) (ffprim(i),i=1,nw)
+        read (neqdsk,2020) (pprime(i),i=1,nw)
+        read (neqdsk,2020) ((psirz(i,j),i=1,nw),j=1,nh)
+        read (neqdsk,2020) (qpsi(i),i=1,nw)
+        read (neqdsk,2022) nbbbs,limitr
+        read (neqdsk,2020) (rbbbs(i),zbbbs(i),i=1,nbbbs)
+        read (neqdsk,2020) (rlim(i),zlim(i),i=1,limitr)
+        c
+        2000 format (6a8,3i4)
+        2020 format (5e16.9)
+        2022 format (2i5)
+```
+"""
 module EQDSKReader
 
-# Write your package code here.
+export
+    Data,
+    read_eqdsk,
+    read_format_2000
+
+# Base.@kwdef
+"""
+    Data
+
+Content structure of EQDSK file.
+
+Attrs:
+
+    case    identification character string
+    nw      number of horizontal grid points
+    nh      number of vertical grid points
+    rdim    horizontal dimension in metter of computational box
+    zdim    vertical dimension in metter of computational box
+    rcentr  R in meter of vacuum toroidal magnetic field BCENTR
+    rleft   Minimum R in meter of rectangular computational box
+    zmid    Z of center of computational box in meter
+    rmaxis  R of magnetic axis in meter
+    zmaxis  Z of magnetic axis in meter
+    simag   poloidal flux at magnetic axis in Weber /rad
+    sibry   poloidal flux at the plasma boundary in Weber /rad
+    bcentr  Vacuum toroidal magnetic field in Tesla at RCENTR
+    current Plasma current in Ampere
+    fpol    Poloidal current function in m-T, F = RB_T on flux grid
+    pres    Plasma pressure in nt / m^2 on uniform flux grid
+    ffprim  FF’(ψ) in (mT)^2 / (Weber /rad) on uniform flux grid
+    pprime  P’(ψ) in (nt/m^2 ) / (Weber /rad) on uniform flux grid
+    psirz   Poloidal flux in Weber / rad on the rectangular grid points
+    qpsi    q values on uniform flux grid from axis to boundary
+    nbbbs   Number of boundary points
+    limitr  Number of limiter points
+    rbbbs   R of boundary points in meter
+    zbbbs   Z of boundary points in meter
+    rlim    R of surrounding limiter contour in meter
+    zlim    Z of surrounding limiter contour in mete
+"""
+struct Data
+    case::String
+    nw::Int16
+    nh::Int16
+    rdim::Float32
+    zdim::Float32
+    rcentr::Float32
+    rleft::Float32
+    zmid::Float32
+    rmaxis::Float32
+    zmaxis::Float32
+    simag::Float32
+    sibry::Float32
+    bcentr::Float32
+    current::Float32
+    fpol::Vector{Float32}
+    pres::Vector{Float32}
+    ffprim::Vector{Float32}
+    pprime::Vector{Float32}
+    psirz::Matrix{Float32}
+    qpsi::Vector{Float32}
+    nbbbs::Int16
+    limitr::Int16
+    rbbbs::Vector{Float32}
+    zbbbs::Vector{Float32}
+    rlim::Vector{Float32} 
+    zlim::Vector{Float32} 
+end
+
+join_lines_skipping_eols(io) = reduce(*, readlines(io))
+read_str(io, nb) = String(Base.read(io, nb))
+read_num(::Type{T}, io, nb) where {T<:Number} = parse(T, read_str(io, nb))
+read_num(t::Type{T}, io, nb, n) where {T<:Number} = (read_num(t, io, nb) for _ in 1:n)
+read_floats(io, n) = read_num(Float32, io, 16, n)
+read_vector(io, n) = Float32[read_floats(io, n)...]
+
+
+function read_eqdsk(io)
+    s = IOBuffer(join_lines_skipping_eols(io))
+    # read (neqdsk,2000) (case(i),i=1,6),idum,nw,nh
+    case = read_str(s, 48)    
+    skip(s, 4)
+    nw, nh = read_num(Int16, s, 4, 2) # row 1
+    # read (neqdsk,2020) rdim,zdim,rcentr,rleft,zmid
+    rdim, zdim, rcentr, rleft, zmid = read_floats(s, 5)  # row 2
+    # read (neqdsk,2020) rmaxis,zmaxis,simag,sibry,bcentr
+    rmaxis, zmaxis, simag, sibry, bcentr = read_floats(s, 5)  # row 3
+    # read (neqdsk,2020) current,simag,xdum,rmaxis,xdum
+    current, simag2, _, rmaxis2, _ = read_floats(s, 5)  # row 4
+    @assert simag2 == simag
+    @assert rmaxis2 == rmaxis
+    # read (neqdsk,2020) zmaxis,xdum,sibry,xdum,xdum
+    zmaxis2, _, sibry2, _, _ = read_floats(s, 5)  # row 5
+    @assert zmaxis2 == zmaxis
+    @assert sibry2 == sibry
+
+    # read (neqdsk,2020) (fpol(i),i=1,nw)
+    fpol = read_vector(s, nw)
+    # read (neqdsk,2020) (pres(i),i=1,nw)
+    pres = read_vector(s, nw)
+    # read (neqdsk,2020) (ffprim(i),i=1,nw)
+    ffprim = read_vector(s, nw)
+    # read (neqdsk,2020) (pprime(i),i=1,nw)
+    pprime = read_vector(s, nw)
+    # read (neqdsk,2020) ((psirz(i,j),i=1,nw),j=1,nh)
+    psirz = reshape(read_vector(s, nw*nh), (nw, nh))
+    # read (neqdsk,2020) (qpsi(i),i=1,nw)
+    qpsi = read_vector(s, nw)
+    # read (neqdsk,2022) nbbbs,limitr
+    nbbbs, limitr = read_num(Int16, s, 5, 2)
+    # read (neqdsk,2020) (rbbbs(i),zbbbs(i),i=1,nbbbs)
+    A = read_vector(s, 2*nbbbs)
+    rbbbs = A[1,:]
+    zbbbs = A[2,:] 
+    # read (neqdsk,2020) (rlim(i),zlim(i),i=1,limitr)
+    A = read_vector(s, 2*limitr)
+    rlim = A[1,:]
+    zlim = A[2,:] 
+
+    EQDSKReader.Data(
+        case,
+        nw,
+        nh,
+        rdim,
+        zdim,
+        rcentr,
+        rleft,
+        zmid,
+        rmaxis,
+        zmaxis,
+        simag,
+        sibry,
+        bcentr,
+        current,
+        fpol,
+        pres,
+        ffprim,
+        pprime,
+        psirz,
+        qpsi,
+        nbbbs,
+        limitr,
+        rbbbs,
+        zbbbs,
+        rlim,
+        zlim
+    )
+end
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,32 @@
 using EQDSKReader
 using Test
 
-@testset "EQDSKReader.jl" begin
-    # Write your tests here.
+HERE = dirname(@__FILE__)
+
+@testset "Read EQDSK file 1" begin
+    input_data = joinpath(HERE, "data", "beforeTQ.eqdsk")
+    data::EQDSKReader.Data = open(read_eqdsk, input_data)
+    @test occursin("disr", data.case)
+    @test data.nw == 65
+    @test data.nh == 129
+    @test data.current == 5.0f6
+    @test data.fpol[1] == -0.178683326f+02   # Note: compare as Float32, this is not equal to Float64 -0.178683326e+02
+    @test data.fpol[end] == -0.172000000f+02
+    @test data.pres[1] == 0.506794023f+06
+    @test data.pres[end] == 0.655331439f+01
+    @test data.ffprim[1] == -0.156641850f+02
+    @test data.ffprim[end] == 0.0
+    @test data.pprime[1] == -0.674156969f+06
+    @test data.pprime[end] == 0.0
+    @test size(data.psirz) == (data.nw, data.nh)
+    @test data.psirz[1,1] == -0.988481275f+00
+    @test data.psirz[data.nw, 1] == -0.685782937f-01
+    @test data.psirz[1,data.nh] == -0.262203469f+00
+    @test data.psirz[data.nw, data.nh] == 0.848762220f+00
+    @test data.qpsi[1] == 0.889066518f+00
+    @test data.qpsi[end] == 0.415200567f+01
+    @test data.nbbbs == 89
+    @test data.limitr == 59
+    # TODO dvp: add other corners
+    @test data.zlim[end] == -0.680490000f+00
 end
